7c6d5644ac16543452f86b461ed827f7ec818842
Aroy <me@aroy-art.com>
2026-09-09 20:06:31 +0200

    chore(deps): update dependency floki to ~> 0.38.0

--- stashix_web/mix.exs
+++ stashix_web/mix.exs
@@ -40,7 +40,7 @@ defmodule Stashix.MixProject do
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.0"},
-      {:floki, "~> 0.36.2"},
+      {:floki, "~> 0.38"},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.5.1", runtime: Mix.env() == :dev},
