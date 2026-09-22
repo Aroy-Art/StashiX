c624014ff425608851c1b893dff071bacdef5e92
Aroy <me@aroy-art.com>
2026-09-22 23:40:08 +0200

    fix(auth): persist session cookie for 7 days to survive PWA restarts

--- stashix_web/lib/stashix_web/endpoint.ex
+++ stashix_web/lib/stashix_web/endpoint.ex
@@ -8,7 +8,8 @@ defmodule StashixWeb.Endpoint do
     store: :cookie,
     key: "_stashix_key",
     signing_salt: "IOjQsDrN",
-    same_site: "Lax"
+    same_site: "Lax",
+    max_age: 7 * 24 * 60 * 60
   ]
 
   socket "/live", Phoenix.LiveView.Socket,
